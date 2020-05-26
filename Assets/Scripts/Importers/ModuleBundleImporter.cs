c7c5d96e227cb52f711bc653ead7988ff62648cf
Elie Michel <elie.michel@exppad.com>
2020-05-26 20:17:19 +0200

    Blender script to export module bundle

---
+++ Assets/Scripts/Importers/ModuleBundleImporter.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.Experimental.AssetImporters;
+using System.IO;
+using Boo.Lang;
+using UnityEditor;
+using System.Linq;
+
+[System.Serializable()]
+class ModuleInfo
+{
+    public string name;
+    public int hash;
+    // + connections
+}
+[System.Serializable()]
+class ModuleBundleInfo
+{
+    public ModuleInfo[] modules;
+    public string fbx_file;
+}
+
+/**
+ * A Module Bundle is a set of metadata around an FBX that is exported from
+ * Blender to Unity.
+ */
+[ScriptedImporter(1, "wfc")]
+public class ModuleBundleImporter : ScriptedImporter
+{
+    public override void OnImportAsset(AssetImportContext ctx)
+    {
+        var root = new GameObject();
+        var bundleInfo = JsonUtility.FromJson<ModuleBundleInfo>(File.ReadAllText(ctx.assetPath));
+
+        string assetDir = Path.GetDirectoryName(ctx.assetPath);
+        string fbxPath = Path.Combine(assetDir, bundleInfo.fbx_file);
+
+        // Load FBX first
+        var fbx = AssetDatabase.LoadAllAssetRepresentationsAtPath(fbxPath);
+        if (fbx == null) return;
+
+        Dictionary<string, MeshFilter> meshFilters = new Dictionary<string, MeshFilter>();
+
+        ctx.DependsOnSourceAsset(fbxPath);
+        foreach (var asset in fbx)
+        {
+            var mf = (asset as GameObject)?.GetComponent<MeshFilter>();
+            if (mf == null) continue;
+            meshFilters[mf.name] = mf;
+        }
+
+        int i = 0;
+        foreach (var moduleInfo in bundleInfo.modules)
+        {
+            Debug.Log("Module '" + moduleInfo.name + "', hash " + moduleInfo.hash);
+            var module = new GameObject(moduleInfo.name);
+            module.transform.SetParent(root.transform);
+            module.transform.position = Vector3.forward * 3 * i;
+            MarchingModule behavior = module.AddComponent<MarchingModule>();
+            behavior.hash = moduleInfo.hash;
+            behavior.allowRotationAroundVerticalAxis = true;
+            if (meshFilters.ContainsKey(moduleInfo.name))
+            {
+                behavior.meshFilter = meshFilters[moduleInfo.name];
+            }
+            ctx.AddObjectToAsset(moduleInfo.name, module);
+            ++i;
+        }
+
+        // 'cube' is a a GameObject and will be automatically converted into a prefab
+        // (Only the 'Main Asset' is elligible to become a Prefab.)
+        ctx.AddObjectToAsset("Module Bundle", root);
+        ctx.SetMainObject(root);
+
+        var material = new Material(Shader.Find("Standard"));
+        material.color = Color.red;
+
+        // Assets must be assigned a unique identifier string consistent across imports
+        ctx.AddObjectToAsset("my Material", material);
+
+        // Assets that are not passed into the context as import outputs must be destroyed
+        var tempMesh = new Mesh();
+        DestroyImmediate(tempMesh);
+    }
+}
